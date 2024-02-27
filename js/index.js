8da8ae5178ac5d8458de72669c3177ea15bc8b9d
Volodymyr Shevchenko <shevchenko.v.vladimirovich@gmail.com>
2024-02-27 20:57:37 +0200

    added calculation of simple expressions, history of calculations

--- js/index.js
+++ js/index.js
@@ -11,16 +11,23 @@ const calcConteiner = document.querySelector(".calc-container");
 //Дисплей калькулятора
 const calcDisplay = document.getElementById("form-control");
 
+//li-шки
+const historyList = document.querySelector(".list-group");
+
 ////////////////// work space //////////////////
 
-console.log(btns);
+// console.log(btns);
 
-console.log(calcConteiner);
+// console.log(calcConteiner);
 
-console.log(calcDisplay);
+// console.log(calcDisplay);
 
 class CalcApp {
-  constructor() {
+  result;
+  event;
+  constructor(calcConteiner, calcDisplay, historyList) {
+    this.clearDisplay();
+
     calcConteiner.addEventListener("click", (e) => {
       e.preventDefault();
       if (
@@ -28,23 +35,48 @@ class CalcApp {
         e.target.classList.contains("zero")
       ) {
         const input = e.target.firstChild.textContent.trim();
-        if (input === "AC") {
+        // console.log(input === "AC");
+        if (input === "AC" && calcDisplay.textContent === "0") {
+          console.log(input === "AC" && calcDisplay.textContent === "0");
+          historyList.innerHTML = "";
+        } else if (input === "AC") {
           this.clearDisplay();
+        } else if (input === ".") {
+          calcDisplay.insertAdjacentText("beforeend", ",");
+        } else if (input === "X") {
+          calcDisplay.insertAdjacentText("beforeend", "*");
+        } else if (input === "=") {
+          this.getDisplayData();
+          // this.addNumberToScreen();
         } else {
           this.addNumberToScreen(input);
         }
 
-        console.log(input.data);
+        // console.log(input);
       }
     });
   }
   clearDisplay() {
-    calcDisplay.innerHTML = "";
+    calcDisplay.innerHTML = "0";
   }
   addNumberToScreen(input) {
-    // calcDisplay.innerHTML = input;
+    if (calcDisplay.textContent === "0") {
+      calcDisplay.innerHTML = "";
+    }
+
     calcDisplay.insertAdjacentText("beforeend", input);
+  }
+
+  getDisplayData() {
+    this.result = calcDisplay.textContent;
+    calcDisplay.innerHTML = eval(this.result);
+    historyList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="list-group-item">${this.result} = ${eval(this.result)}</li>`
+    );
+
+    console.log(this.result);
   }
 }
 
-const calculatorApp = new CalcApp();
+const calculatorApp = new CalcApp(calcConteiner, calcDisplay, historyList);
